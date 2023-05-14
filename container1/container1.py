812783f85dc34fe8ef74838587a1bc82324b0a1d
Krishna Modi <kr733081@dal.ca>
2023-05-13 21:54:50 -0300

    Logic for Container 1

--- container1/container1.py
+++ container1/container1.py
@@ -1,6 +1,40 @@
-from flask import Flask
+from flask import Flask,jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/calculate", method=['POST'])
 def calculate():
-    return "Setting up container 1"
+    data= request.get_json()
+
+    #if the file name is not provided an error message "Invalid JSON input." is returned
+    if "file" not in data or data["file"] is None:
+        return jsonify(
+            {
+                "file": None,
+                "error": "Invalid JSON input."
+            }
+        )
+    
+    #if filename is provided but not found mounted on disk voulme an error message is "File not found." is returned
+    file=data["file"]
+    try: 
+        open(file)
+    except FileNotFoundError:
+        return jsonify(
+            {
+                "file": file,
+                "error": "File not found."
+            }
+        )
+    
+    #Container 1 sends input to Container 2
+    product=data["product"]
+    container2="http://localhost:6000/sum"
+
+    response = request.post(container2,json={
+        "file": file,
+        "product": product
+    })
+    return response.json()
+
+if __name__ == 'main':
+    app.run(host='0.0.0.0', port=6000 )
